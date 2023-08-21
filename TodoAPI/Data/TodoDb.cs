af03c72620c776d5ecf64bc9b207e054f73b1808
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-08-21 14:15:04 -0300

    :sparkles: feat TodoAPI - Minimal Api

---
+++ TodoAPI/Data/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI.Model;
+
+namespace TodoAPI.Data
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {            
+        }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
