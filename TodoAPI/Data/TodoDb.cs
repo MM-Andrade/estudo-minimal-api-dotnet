50ba4f35a275515fac63489ba5218e9313af153b
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-08-21 16:40:08 -0300

    :sparkles: feat Sqlite / Mapgroup API

--- TodoAPI/Data/TodoDb.cs
+++ TodoAPI/Data/TodoDb.cs
@@ -11,4 +11,6 @@ namespace TodoAPI.Data
 
         public DbSet<Todo> Todos => Set<Todo>();
     }
+
+
 }
---
+++ TodoAPI/Extensions/DbExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI.Data;
+
+namespace TodoAPI.Extensions
+{
+    public static class DbExtensions
+    {
+        public static void AddDbServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var sectionName = "TodoDatabase";
+            var connectionStringDb = configuration.GetConnectionString(sectionName);
+
+            services.AddDbContext<TodoDb>(options => {
+                options.UseSqlite(connectionStringDb);
+            });
+
+        }
+    }
+}
